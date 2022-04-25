57df8fbbe7db2b3a1a3eea77ba8d216b9c0cea8e
hfgd123 <46094961+hfgd123@users.noreply.github.com>
2022-04-25 17:14:40 +0200

    Ping

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ function onMessageHandler(target, tags, message, self){
         client.say(target, "https://store.sc-network.net");
     } else if(splitMessage[0] === "!info"){
         client.say(target, "Ich bin ein Bot, der von @hfgd_gaming in Node.js gemacht wurde. Meinen Code findest du auf https://github.com/hfgd123/twitch-bot. Solltest du dich damit auskennen, klicke da aber bitte nicht drauf. Augenkrebsgefahr!");
+    } else if(splitMessage[0] === "!ping"){
+        client.say(target, "Pong! Ja, das geht. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et ju");
     }
 }
 function onConnectedHandler (addr, port) {
